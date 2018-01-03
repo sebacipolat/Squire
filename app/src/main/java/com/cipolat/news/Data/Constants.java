357d2342861ce1305e86d4365210363c195117af
scipolat <scipolat@cmd.com.ar>
2018-01-02 23:22:07 -0300

    modify

--- app/src/main/java/com/cipolat/news/Data/Constants.java
+++ app/src/main/java/com/cipolat/news/Data/Constants.java
@@ -6,6 +6,6 @@ package com.cipolat.news.Data;
 
 public class Constants {
     public static String API_URL="https://content.guardianapis.com/";
-    public static String API_KEY="061c6169-d4bd-4732-b600-f38e16e26515";
+    public static String API_KEY="YOUR GUARDIAN KEY";
 
 }
